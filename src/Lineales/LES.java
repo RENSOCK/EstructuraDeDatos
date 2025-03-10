e14dadef6286e3da9e4771e17b79a4d78ca39acf
RENSOCK <manuel_rios7@hotmail.com>
2025-03-09 18:08:07 -0700

    hacemos un metodo para poder asi agregar un nodo nuevo, de tal manera que nodo nuevo que entre se convierta en el primero de la lista

--- src/Lineales/LES.java
+++ src/Lineales/LES.java
@@ -33,5 +33,23 @@ public boolean isEmpty() {
 public int size() {
     return tam;  // Retorna el valor de 'tam', que representa la cantidad de elementos en la lista.
 }
-
+/**
+ * Añade un nodo al inicio de la lista.
+ *
+ * @param nN El nodo a ser añadido al principio de la lista.
+ * @return true si el nodo fue añadido correctamente, false en caso de algún error.
+ */
+public boolean addFirst(Nodo nN) {
+    if (this.isEmpty()) {  // Si la lista está vacía (Inicial es null)
+        Inicial = nN;  // El nodo se convierte en el primer nodo de la lista.
+        tam++;  // Se incrementa el tamaño de la lista.
+        return true;  // El nodo fue añadido correctamente.
+    } else if (size() > 0) {  // Si la lista no está vacía
+        nN.setSiguiente(Inicial);  // El nuevo nodo apunta al nodo que estaba al principio.
+        Inicial = nN;  // Ahora, el nuevo nodo es el primero en la lista.
+        tam++;  // Se incrementa el tamaño de la lista.
+        return true;  // El nodo fue añadido correctamente.
+    }
+    return false;  // Si no se pudo añadir el nodo, retorna false.
+}
 }
