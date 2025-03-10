6ea1e59507453ade83495910f0aab43d802abe2d
RENSOCK <manuel_rios7@hotmail.com>
2025-03-09 18:00:57 -0700

    Hacemos un metodo que nos permita conocer si la lista esta vacia, regresa false en caso de que contenga algun nodo

--- src/Lineales/LES.java
+++ src/Lineales/LES.java
@@ -13,9 +13,17 @@ import Nodos.Nodo;
 public class LES {
     
     /**
-     *
+     *Inicializa los valores
      */
     private Nodo Inicial;
     private int tam;
+/**
+ * Nos permite conocer si la lista esta (true) vacía o no (false)
+ *
+ * @return true si "Inicial" es nulo (la lista está vacía), false en caso de que contenga algún nodo.
+ */
+public boolean isEmpty() {
+    return Inicial == null;  // Devuelve true si 'Inicial' es null, indicando que la lista está vacía.
+}
 
 }
